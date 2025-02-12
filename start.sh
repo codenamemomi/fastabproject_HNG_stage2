b86b38609d134deddaf826baf0290d1744d1b80a
codenamemomi <akinrogundecodenamemomi@gmail.com>
2025-02-12 22:49:46 +0100

    deployment error fix 32

--- start.sh
+++ start.sh
@@ -4,7 +4,8 @@ echo "Starting the application..."
 echo "Current working directory: $(pwd)"
 
 echo "Starting Nginx..."
-nginx -c $(pwd)/nginx.conf -g "daemon off;" &  # Start Nginx in the background
+nginx -c $(pwd)/nginx/nginx.conf -g "daemon off;" &
+
 
 echo "Starting FastAPI..."
 uvicorn main:app --host 0.0.0.0 --port 8000
