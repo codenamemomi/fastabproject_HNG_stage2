a6562dead6d824d1f5ea956a47028cbf38a663f4
codenamemomi <akinrogundecodenamemomi@gmail.com>
2025-02-12 19:00:06 +0100

    deployment error fix 2

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ pwd
 mkdir -p /home/runner/work/fastabproject_HNG_stage2/tmp
 
 # Start Nginx
-nginx -c /home/runner/work/fastabproject_HNG_stage2/nginx/nginx.conf
+nginx -c ./nginx/nginx.conf
 
 # Start FastAPI
 uvicorn main:app --host 0.0.0.0 --port 8000
