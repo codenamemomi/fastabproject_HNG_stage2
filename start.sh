410c47b4c1fe70e4dba50fbbec88ccef3c14a0de
codenamemomi <akinrogundecodenamemomi@gmail.com>
2025-02-12 18:39:04 +0100

    edited deploy.yml and start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "Current working directory:"
 pwd
 
 # Start Nginx
-nginx -c ./nginx/nginx.conf
+nginx -c /home/runner/work/fastabproject_HNG_stage2/fastabproject_HNG_stage2/nginx/nginx.conf
 
 # Start FastAPI
 uvicorn main:app --host 0.0.0.0 --port 8000
