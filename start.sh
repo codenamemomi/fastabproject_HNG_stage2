32904548f6bdcea95728772eff2083a4028b2439
codenamemomi <akinrogundecodenamemomi@gmail.com>
2025-02-12 18:19:43 +0100

    edited deploy.yml and start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 
 # Start Nginx
-nginx -c /app/nginx/nginx.conf
+nginx -c nginx/nginx.conf
 
 # Start FastAPI
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+uvicorn main:app --host 0.0.0.0 --port 8000
