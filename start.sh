613574aa111fc40dea04f58cf7bdec511abbacc7
codenamemomi <akinrogundecodenamemomi@gmail.com>
2025-02-13 04:04:39 +0100

    Trigger full redeploy

--- start.sh
+++ start.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-echo "Starting the application..."
-echo "Current working directory: $(pwd)"
 
 echo "Starting FastAPI..."
-uvicorn main:app --host 0.0.0.0 --port 8000 &  # Run FastAPI in the background
+uvicorn main:app --host 0.0.0.0 --port 8000 &  
 
 # Wait for FastAPI to become available
 while ! curl -s http://127.0.0.1:8000/healthcheck; do
