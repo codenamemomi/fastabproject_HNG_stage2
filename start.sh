4500542a434f013c9f121fc9c9e91ab8d6e4a05b
codenamemomi <akinrogundecodenamemomi@gmail.com>
2025-02-13 04:30:33 +0100

    Trigger full redeploy

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 echo "Starting FastAPI..."
-uvicorn main:app --host 0.0.0.0 --port 8000 &  
-
-# Wait for FastAPI to become available
-while ! curl -s http://127.0.0.1:8000/healthcheck; do
-  echo "Waiting for FastAPI to start..."
-  sleep 2
-done
-
-echo "Starting Nginx..."
+uvicorn main:app --host 0.0.0.0 --port 8000
