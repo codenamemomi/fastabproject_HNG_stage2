197c462eec320f80ba1989cfced408ff2859b5c7
codenamemomi <akinrogundecodenamemomi@gmail.com>
2025-02-14 13:56:53 +0100

    ci pipeline test 9

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app.add_middleware(
 app.include_router(api_router, prefix=settings.API_PREFIX)
 
 
-@app.get("/stage2")
+@app.get("/healthcheck")
 async def health_check():
     """Checks if server is active."""
     return {"status": "active"}
